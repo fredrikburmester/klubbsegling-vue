49cad97654df9a7ea37fc92669d702482861012b
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-01-22 13:47:12 +0100

    chore: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,tsx}'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {
