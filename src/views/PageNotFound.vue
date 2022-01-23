857c0d3f5c4d3bbb467230410eefb08b03d2b98d
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-01-23 13:12:20 +0100

    404 page

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,7 +1,8 @@
 <template>
-	<div class="flex justify-self-center text-center">
-		<h1>404</h1>
-		<h2>Kunde inte hitta sidan</h2>
+	<div class="flex flex-col justify-self-center text-center h-96 mt-52 mb-12">
+		<h1 class="mb-4 text-5xl font-bold">404</h1>
+		<h2 clas="mb-4">Kunde inte hitta sidan</h2>
+		<router-link to="/" class="mt-4"><span class="underline"><font-awesome-icon icon="home" class="mr-2" />Gå hem</span></router-link>
 	</div>
 </template>
 
