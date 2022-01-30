abd340b934548206016c8b7246856135f74621fa
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-01-30 18:57:36 +0100

    design fixes

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,15 +1,16 @@
 <template>
-    <div class="max-w-2xl px-6 pt-6 justify-self-center grid mb-16" v-if="!loading">
-        <h1 class="text-3xl font-bold mb-4">{{ article.title }}</h1>
-        <h1 class="text-xl font-bold">{{ article.subtitle || '' }}</h1>
-        <div class="flex flex-row">
-            <p class="mr-2">Skriven av:</p>
-            <div class="badge badge-info mr-2" v-for="author in article.authors.data">{{ author.attributes.firstName }} {{ author.attributes.lastName }}</div>
+    <div id="article" class="max-w-2xl px-6 pt-14 justify-self-center grid mb-16" v-if="!loading">
+        <h1 class="text-3xl font-bold mb-2">{{ article.title }}</h1>
+        <h1 class="text-xl text-gray-600 italic mb-2">{{ article.subtitle || '' }}</h1>
+        <hr class="my-4" />
+        <p class="mr-2 mt-2">Skriven av:</p>
+        <div class="flex mb-2 flex-wrap">
+            <div class="badge badge-info mr-2 mt-2" v-for="author in article.authors.data">{{ author.attributes.firstName }} {{ author.attributes.lastName }}</div>
         </div>
-        <p class="text-sm italic">{{ article.publishedAt }}</p>
+        <p class="text-sm italic">Publicerad: {{ formatDate(article.publishedAt) }}</p>
         <hr class="my-4" />
         <Markdown :source="article.body" class="mb-8" />
-        <div class="carousel rounded-box">
+        <div class="p-4 space-x-4 carousel carousel-center bg-blue-400 rounded-box">
             <div v-for="image in article.images.data" class="carousel-item">
                 <img :src="`https://cms.klubbsegling.se${image.attributes.formats.small.url}`" />
             </div>
@@ -39,5 +40,16 @@ export default {
             this.loading = false
         })
     },
+    methods: {
+        formatDate(date) {
+            return new Date(date).toLocaleDateString('sv-SE')
+        },
+    },
 }
 </script>
+
+<style>
+#article {
+    font-family: 'Times New Roman', Times, serif;
+}
+</style>
