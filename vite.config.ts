6ad19612c2be3063eff1c38e11ab4e74d8ad1da8
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-01-30 13:22:18 +0100

    lint

--- vite.config.ts
+++ vite.config.ts
@@ -5,41 +5,37 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-	plugins: [
-		vue(),
-		VitePWA({
-			includeAssets: [
-				'favicon.ico',
-				'robots.txt',
-				'apple-touch-icon.png',
-			],
-			manifest: {
-				name: 'Klubbsegling',
-				short_name: 'Klubbsegling',
-				description: 'Sveriges bästa klubbseglings hemsida!',
-				theme_color: '#00428C',
-				icons: [
-					{
-						src: 'android-chrome-192x192.png',
-						sizes: '192x192',
-						type: 'image/png',
-					},
-				],
-			},
-		}),
-	],
-	define: {
-		'process.env': {},
-	},
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, '/src'),
-		},
-	},
-	css: {
-		preprocessorOptions: {},
-	},
-	build: {
-		minify: true,
-	},
+    plugins: [
+        vue(),
+        VitePWA({
+            includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+            manifest: {
+                name: 'Klubbsegling',
+                short_name: 'Klubbsegling',
+                description: 'Sveriges bästa klubbseglings hemsida!',
+                theme_color: '#00428C',
+                icons: [
+                    {
+                        src: 'android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
+    ],
+    define: {
+        'process.env': {},
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, '/src'),
+        },
+    },
+    css: {
+        preprocessorOptions: {},
+    },
+    build: {
+        minify: true,
+    },
 })
