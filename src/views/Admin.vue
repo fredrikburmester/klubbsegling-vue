823149def784bc96e543b5892790bf6bbe6bed04
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-01-30 12:17:56 +0100

    draft admin page

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,5 +1,36 @@
 <template>
-    <div class="about">
-        <h1>This is an profile proteted page</h1>
+    <div class="wrapper max-w-2xl justify-self-center px-6 pt-6">
+        <router-link to="w">
+            <button class="btn">Skapa Tävling</button>
+        </router-link>
     </div>
 </template>
+
+<script>
+import { API_URL } from '@/store/actions/auth'
+import axios from 'axios'
+import qs from 'qs'
+
+export default {
+    mounted() {
+        const query = qs.stringify(
+            {
+                populate: ['role'],
+            },
+            {
+                encodeValuesOnly: true,
+            }
+        )
+        const headers = {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+            },
+        }
+        axios.get(`${API_URL}/users/me?${query}`, headers).then(res => {
+            console.log(res.data)
+        })
+        var a = this.$store.getters.getProfile
+        console.log(a)
+    },
+}
+</script>
