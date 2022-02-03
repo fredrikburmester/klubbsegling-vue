c3d1b4dbb54a8c57b7ae3485429615f323494db2
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-02-03 12:03:15 +0100

    bug fixes

--- src/components/ReloadPWA.vue
+++ src/components/ReloadPWA.vue
@@ -9,37 +9,32 @@
                     d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
                 ></path>
             </svg>
-            <label class="mx-3">Hemsidan har en uppdatering. {{ needRefresh }}</label>
+            <label class="mx-3">Hemsidan har en uppdatering. Klicka uppdatera och sen ladda om.</label>
         </div>
         <div class="flex-none">
-            <button @click="updateServiceWorker()" class="btn btn-sm btn-primary">Uppdatera</button>
+            <button @click="update" class="btn btn-sm btn-primary mr-2">Uppdatera</button>
+            <button @click="reload" class="btn btn-sm btn-gray-500">Ladda om</button>
         </div>
     </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { useRegisterSW } from 'virtual:pwa-register/vue'
-const { updateServiceWorker } = useRegisterSW()
 export default defineComponent({
     name: 'ReloadPWA',
     setup() {
-        const { offlineReady, needRefresh, updateServiceWorker } = useRegisterSW()
-        const close = async () => {
-            offlineReady.value = false
-            needRefresh.value = false
-        }
-        return { offlineReady, needRefresh, updateServiceWorker, close }
+        const { needRefresh, updateServiceWorker } = useRegisterSW()
+        return { needRefresh, updateServiceWorker }
     },
     methods: {
-        async close() {
-            // @ts-ignore
-            this.offlineReady.value = false
-            // @ts-ignore
-            this.needRefresh.value = false
+        async update() {
+            console.log('[0] updateServiceWorker')
+            await this.updateServiceWorker()
+            console.log('[1] updateServiceWorker')
+            this.$router.go(0)
         },
-        async updateServiceWorker() {
-            await updateServiceWorker()
-            window.location.href = 'https://klubbsegling.se'
+        async reload() {
+            this.$router.go(0)
         },
     },
 })
