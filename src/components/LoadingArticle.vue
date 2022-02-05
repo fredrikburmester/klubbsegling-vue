4dbaea40fd4f76e61bcb4432985169345c4569a1
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-02-05 16:38:57 +0100

    design updates

--- src/components/LoadingArticle.vue
+++ src/components/LoadingArticle.vue
@@ -1,26 +1,28 @@
 <template>
-    <div class="wrapper px-6 max-w-md mt-8 lg:mt-12 animate-pulse">
+    <div class="wrapper justify-self-center px-6 max-w-2xl mt-8 lg:mt-12 animate-pulse mb-12">
         <div class="placeholder-h1 bg-gray-200 mb-2 w-64 rounded-lg"></div>
         <div class="placeholder-h1 bg-gray-200 mb-4 w-32 rounded-lg"></div>
         <div class="h-2 bg-gray-200 mb-4 rounded-lg w-32"></div>
         <div class="h-2 bg-gray-200 mb-6 rounded-lg"></div>
-        <div class="h-96 bg-gray-100 mb-4 p-4 rounded-lg">
-            <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
-            <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
-            <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
-            <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
-            <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
-            <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
-            <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
-            <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
-            <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
-            <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
-            <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
-            <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
-            <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
-            <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
-            <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
-        </div>
+        <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
+        <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
+        <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
+        <div class="h-2 bg-gray-200 mb-4 rounded-lg 2-64"></div>
+        <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
+        <div class="h-2 bg-gray-200 mb-4 rounded-lg w-48"></div>
+        <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
+        <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
+        <div class="h-2 bg-gray-200 mb-4 rounded-lg 2-64"></div>
+        <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
+        <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
+        <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
+        <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
+        <div class="h-2 bg-gray-200 mb-4 rounded-lg w-64"></div>
+        <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
+        <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
+        <div class="h-2 bg-gray-200 mb-4 rounded-lg w-48"></div>
+        <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
+        <div class="h-2 bg-gray-200 mb-4 rounded-lg"></div>
     </div>
 </template>
 
